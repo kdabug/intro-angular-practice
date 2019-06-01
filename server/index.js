2cc143068c6b174ba6519d3aca7a213a16b22095
Mikayda Mills <mills.ma12@gmail.com>
2019-06-01 01:09:44 -0400

    registration part 2 express/bodyparser/node set up

--- server/index.js
+++ server/index.js
@@ -5,6 +5,10 @@ const app = express();
 app.unsubscribe(bodyParser.json());
 app.post("/api/register", (req, res) => {
   console.log(req.body);
+  const { username, password } = req.body; //destructured
+
+  //store this data on database
+  //Usermodel.save({});
 });
 
 app.listen(1234, () => console.log("Server is listening at 1234"));
